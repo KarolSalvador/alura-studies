4b18497db1ff9ec44baa7b86a469b9b947809f07
Karol <karolsalvador91@gmail.com>
2024-12-05 10:31:05 -0300

    adicionando atualização da tarefa

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,6 +1,8 @@
+import { Itarefa } from '../../../types/tarefas'
 import style from '../Lista.module.scss'
 
-export default function item ({ tarefa, tempo }: { tarefa: string, tempo: string }) {
+export default function item ({ tarefa, tempo, selecionado, completado, id }: Itarefa) {
+    console.log('item atual: ', {tarefa, tempo, selecionado, completado, id})
     return (
         <li className={style.item}>
             <h3>{tarefa}</h3>
