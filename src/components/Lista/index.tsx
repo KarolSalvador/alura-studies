00132d568e3dc9e14105fba6820bb6c2e56d9b2e
Karol <karolsalvador91@gmail.com>
2024-12-05 15:22:23 -0300

    Seleção de tarefa selecionada

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,17 +1,23 @@
-import { Itarefa } from '../../types/tarefas';
+import { ITarefa } from '../../types/tarefas';
 import Item from './Item';
 import style from './Lista.module.scss';
 
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
 
-function Lista({ tarefas }: { tarefas: Itarefa[]}) {
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
     
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => (  //.map retorna um JSX conforme indicado, no caso um li 
+                {tarefas.map(item => (  //.map retorna um JSX conforme indicado, no caso um li 
                     <Item 
-                        key={index}
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
                         {...item}
                     />
                 ))}
